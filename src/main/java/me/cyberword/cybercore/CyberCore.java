938727b3302975c66a112b9b7ec7ac3817120116
CyberWordPL <cyberwordixx@gazeta.pl>
2025-02-26 17:04:43 +0100

    add config system

--- src/main/java/me/cyberword/cybercore/CyberCore.java
+++ src/main/java/me/cyberword/cybercore/CyberCore.java
@@ -2,12 +2,25 @@ package me.cyberword.cybercore;
 
 import me.cyberword.cybercore.listeners.ListenersFetcher;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CyberCore extends JavaPlugin {
+    private static CyberCore instance;
+    private FileConfiguration config;
+
+    public static CyberCore getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        config = getConfig();
+
         for(Listener listener : ListenersFetcher.getListeners()) {
             Bukkit.getPluginManager().registerEvents(listener, this);
         }
@@ -15,5 +28,6 @@ public final class CyberCore extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        saveConfig();
     }
 }
--- src/main/java/me/cyberword/cybercore/gui/items/IInventoryGuiItem.java
+++ src/main/java/me/cyberword/cybercore/gui/items/IInventoryGuiItem.java
@@ -1,15 +1,13 @@
 package me.cyberword.cybercore.gui.items;
 
 import me.cyberword.cybercore.gui.IItemSystemInventoryGui;
-import me.cyberword.cybercore.gui.InventoryGui;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
 
 public interface IInventoryGuiItem {
     void setItem(ItemStack stack);
     ItemStack getItem();
 
-    void inventoryClickEventHandler(InventoryClickEvent event, IItemSystemInventoryGui gui)
+    void inventoryClickEventHandler(InventoryClickEvent event, IItemSystemInventoryGui gui);
 }
